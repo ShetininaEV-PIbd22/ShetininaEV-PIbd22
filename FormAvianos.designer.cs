0cb299538358859fa87531a9c4c4478bf4960e07
ShetininaEV <55050874+ShetininaEV-PIbd22@users.noreply.github.com>
2020-01-17 15:07:52 +0400

    Update FormAvianos.designer.cs

--- FormAvianos.designer.cs
+++ FormAvianos.designer.cs
@@ -29,10 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAvianos));
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.pictureBoxAvianos = new System.Windows.Forms.PictureBox();
             this.CreateShep = new System.Windows.Forms.Button();
             this.buttonUp = new System.Windows.Forms.Button();
@@ -42,46 +38,6 @@
             this.CreateAvianos = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAvianos)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(659, 425);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.buttonMove_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(598, 396);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.buttonMove_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(724, 396);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.buttonMove_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(659, 367);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.buttonMove_Click);
             // 
             // pictureBoxAvianos
             // 
@@ -172,10 +128,6 @@
             this.Controls.Add(this.buttonUp);
             this.Controls.Add(this.CreateShep);
             this.Controls.Add(this.pictureBoxAvianos);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
             this.Name = "FormAvianos";
             this.Text = "Avianosec";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAvianos)).EndInit();
@@ -184,10 +136,6 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.PictureBox pictureBoxAvianos;
         private System.Windows.Forms.Button CreateShep;
         private System.Windows.Forms.Button buttonUp;
