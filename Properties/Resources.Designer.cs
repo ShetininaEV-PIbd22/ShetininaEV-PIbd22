5fa540f23365a0c25420afd20590625176e320d0
ShetininaEV-PIbd22 <shetinina.l2000@gmail.com>
2020-01-17 18:43:09 +0400

    laba 3 new

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -44,7 +44,7 @@ namespace WindowsFormsAppAvianos.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WindowsFormsAppNasta.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WindowsFormsAppAvianos.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
